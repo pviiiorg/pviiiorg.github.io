573d3fd8b91ae53af28c490f5c37f422a4c825d7
James Padolsey <github@padolsey.net>
2016-12-07 09:15:00 +0000

    Update inject.js

--- 2016-ip-act-blackout/inject.js
+++ 2016-ip-act-blackout/inject.js
@@ -3,7 +3,7 @@
 	var i = document.body.appendChild(document.createElement('iframe'));
 	i.setAttribute('allowTransparency', 'true');
 	i.style = 'position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 2147483647;';
-	i.src = 'https://files.pviii.org/2016-ip-act-blackout/';
+	i.src = 'https://pviii.org/2016-ip-act-blackout/';
 	window.addEventListener('message', handler);
 	function handler(e) {
 		if (e.data === '__ip_act_iframe_close') {
