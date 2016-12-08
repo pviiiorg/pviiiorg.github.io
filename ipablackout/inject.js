66aa9e93614e75c07c2acdebb203bec3d1f860ee
James Padolsey <github@padolsey.net>
2016-12-08 22:19:59 +0000

    various

--- ipablackout/inject.js
+++ ipablackout/inject.js
@@ -1,9 +1,16 @@
 (function() {
 	if (typeof window.postMessage != 'function') return;
 	var i = document.documentElement.appendChild(document.createElement('iframe'));
+	var s = document.getElementsByTagName('script');
+	var page = getOp('page') || 'full.html';
+	var height = getOp('height') || '100%';
+	function getOp(n) {
+		return s[s.length - 1].getAttribute('data-' + n);
+	}
 	i.setAttribute('allowTransparency', 'true');
-	i.style = 'position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 2147483647;';
+	i.style = 'position: fixed; top: 0; left: 0; width: 100%; z-index: 2147483647;';
 	i.src = 'https://pviii.org/2016-ip-act-blackout/full.html';
+	i.style.height = height;
 	window.addEventListener('message', handler);
 	function handler(e) {
 		if (e.data === '__ip_act_iframe_close') {
